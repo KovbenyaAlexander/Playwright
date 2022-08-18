002c7b886372558d5cafe6a0f874981baa43654e
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-08-18 21:35:43 +0300

    add a solution for the second task

---
+++ tests/task2.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+
+test("The page should display the current time after submit", async ({
+  page,
+}) => {
+  await page.goto(
+    "http://89.189.152.235:1337/?path=/story/eos-timepicker--in-form"
+  );
+
+  const frame = page.frameLocator("#storybook-preview-iframe");
+  const showTimePicker = frame.locator(".eos-body .eos-field-suffix-icon");
+  await showTimePicker.click();
+
+  const pickCurrentTime = frame.locator(".eos-picker-now-btn");
+  const date = new Date();
+  await pickCurrentTime.click();
+
+  const timeInput = frame.locator(".eos-picker-input #basic_timepicker");
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const currentTime = `${hours}:${minutes}`;
+  await expect(timeInput).toHaveValue(currentTime);
+
+  const submitButton = frame.locator("button", { hasText: "SUBMIT" });
+  await submitButton.click();
+
+  const messageContainer = frame.locator(".eos-message-notice");
+  await expect(messageContainer).toBeVisible();
+});
+
+/*
+
+Тест 2.
+Шаги:
+1. Перейти на страницу http://89.189.152.235:1337/?path=/story/eos-timepicker--in-form
+2. Раскрыть список
+3. Нажать кнопку "Сейчас"
+4. Нажать кнопку "Submit"
+
+Ожидаемый результат:
+1. Отобразилось сообщение с текущим временем
+2. Поле class="eos-picker-input" содержит текущее время
+
+*/
