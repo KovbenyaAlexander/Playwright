a4298d01db72b4b90e9058a61ea16743cb584178
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-08-18 19:37:15 +0300

    add a solution for the first task

---
+++ tests/task1.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("Tab number 7 should contain the text 'Текст 7'", async ({ page }) => {
+  await page.goto("http://89.189.152.235:1337/?path=/story/eos-tabs--default");
+
+  const frame = page.frameLocator("#storybook-preview-iframe");
+  const tab = frame.locator("text=Вкладка 7");
+  tab.click();
+
+  const textContainer = frame.locator(".eos-tabs-tabpane-active");
+
+  await expect(textContainer).toHaveText("Текст 7");
+});
+
+/*
+
+Тест 1.
+Шаги:
+1. Перейти на страницу http://89.189.152.235:1337/?path=/story/eos-tabs--default
+2. Перейти на "Вкладка 7"
+
+Ожидаемый результат:
+1. Проверить наличие "Текст 7"
+
+*/
